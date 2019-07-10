aabd08c2f8f9521f372ad6291169db91fef14c61
Frank S. Bara <fbara1@yahoo.com>
2019-07-10 13:29:18 -0500

    First GitHub commit.

--- FBGroups/ContentView.swift
+++ FBGroups/ContentView.swift
@@ -9,10 +9,36 @@
 import SwiftUI
 
 struct ContentView : View {
+    let posts = ["1", "2", "3","1", "2", "3"]
+    
     var body: some View {
         NavigationView {
             List {
-                Text("First row")
+                
+                
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        Text("Trending")
+                        HStack {
+                            Text("Group 1")
+                            Text("Group 1")
+                            Text("Group 1")
+                            Text("Group 1")
+                            Text("Group 1")
+                            Text("Group 1")
+                            Text("Group 1")
+                            Text("Group 1")
+                            Text("Group 1")
+                            Text("Group 1")
+                        }
+                    }
+                    
+                }.frame(height: 150)
+                
+                ForEach(posts.identified(by: \.self)) { post in
+                    Text(post)
+                }
+
             }.navigationBarTitle(Text("Groups"))
         }
     }
