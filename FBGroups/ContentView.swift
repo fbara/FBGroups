ae436b6d7d57c8610e592e96ecb6f684211eee4f
Frank S. Bara <fbara1@yahoo.com>
2019-07-11 21:44:40 -0500

    Get UI on main screen close to what I want.

--- FBGroups/ContentView.swift
+++ FBGroups/ContentView.swift
@@ -28,19 +28,51 @@ struct ContentView : View {
                             Text("Group 1")
                             Text("Group 1")
                             Text("Group 1")
-                            Text("Group 1")
-                            Text("Group 1")
                         }
                     }
                     
-                }.frame(height: 150)
+                }.frame(height: 100)
                 
+                // post rows
                 ForEach(posts.identified(by: \.self)) { post in
-                    Text(post)
+                    PostView()
                 }
 
             }.navigationBarTitle(Text("Groups"))
         }
+    }
+}
+
+struct PostView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 12) {
+            HStack {
+                Image("cappuccino")
+                    .resizable()
+                    .clipShape(Circle())
+                    .frame(width: 60, height: 40)
+                    .clipped()
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Donald Trump")
+                            .font(.headline)
+                        Text("Posted 8 hours ago")
+                            .font(.subheadline)
+                    }
+                    .padding(.leading, 8)
+            }
+            .padding(.leading, 16)
+            .padding(.top, 16)
+            Text("Post body text")
+                .lineLimit(nil)
+                .padding(.leading, 16)
+                .padding(.trailing, 32)
+            
+            Image("mocha")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+        }.padding(.leading, -20)
+        .padding(.bottom, -8)
+
     }
 }
 
